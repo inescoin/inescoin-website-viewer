fc83129ec43780c59b8320495c1b774006412d36
Mounir R'Quiba <mounir.rquiba@gmail.com>
2022-04-18 17:36:52 +0200

    Favicon added

--- src/template/wrapper.tpl.php
+++ src/template/wrapper.tpl.php
@@ -13,6 +13,13 @@ if (empty($domain)) {
   <meta charset="utf-8">
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-sc ale=1, shrink-to-fit=no">
+
+  <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/favicon/site.webmanifest">
+
   <?php if (!empty($domain['website']['meta'])) {
           foreach ($domain['website']['meta'] as $meta) { ?>
   <meta <?php echo $meta['type']; ?>="<?php echo $meta['name']; ?>" content="<?php echo $meta['content']; ?>">
